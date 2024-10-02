8f39755aa250c7c82be59363d534e8dae194c9db
Miguel-León <159438587+Sefirr-dot@users.noreply.github.com>
2024-10-02 18:10:09 +0200

    02 Octubre

---
+++ UT1_EjemplosClase/src/ejerciciosEjemplo/ExportarAFile.java
@@ -0,0 +1,32 @@
+package ejerciciosEjemplo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+public class ExportarAFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		try (InputStreamReader ipsr = new InputStreamReader(System.in, Charset.forName("UTF-8"));
+		BufferedReader bReader = new BufferedReader(ipsr);
+		
+		FileWriter fw = new FileWriter(new File("ExportarAFile.txt"));
+		BufferedWriter bWriter = new BufferedWriter(fw)) {
+			String lineString = "";
+			do {
+				lineString = bReader.readLine();
+				bWriter.write(lineString);
+				bWriter.newLine();
+			} while(!lineString.isBlank());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println(ex.getMessage());
+		}
+	}
+
+}
